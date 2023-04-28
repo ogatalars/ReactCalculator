f062b992323250945f6f033f130bea3dfb5d5905
ogatalars <ogataogara@gmail.com>
2023-04-28 18:37:48 -0300

    Add: buttons

---
+++ src/DigitalOperationalButton.tsx
@@ -0,0 +1,30 @@
+import styled from "@emotion/styled";
+import {Button, Grid } from "@mui/material";
+
+interface DigitalOperationalButtonProps {
+    operational: string;
+    selectOperation: (operational: string) => void;
+    selectedOperation: string;
+}
+
+const StyledButton = styled(Button)<{selected: boolean} > ((props) => ({
+    backgroundColor: "rgb(254, 241, 73,.1)", 
+    borderColor: props.selected ? "rgb(254, 241, 73)" : "rgb(254, 241, 73,.1)",
+}))
+
+export const DigitalOperationalButton: React.FC<DigitalOperationalButtonProps> = ({
+    operational, 
+    selectedOperation, 
+    selectOperation
+}) => {
+    return (
+        <Grid item>
+            <StyledButton fullWidth variant="outlined" onClick={() => selectOperation(operational)}
+            selected={selectedOperation === operational} 
+            >
+         {operational}
+        
+        </StyledButton>
+        </Grid>
+    )
+}
