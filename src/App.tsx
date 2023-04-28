9eb00ae1ae1abc82987b333463f337cfe3e45a48
ogatalars <ogataogara@gmail.com>
2023-04-28 14:37:06 -0300

    Add: Output number

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
-import {Container, Paper, styled} from "@mui/material"
+import {Container, Paper,Grid, styled} from "@mui/material"
 
+const OutPutContainer = styled('div')(({theme})=> ({
+  width: "90%", 
+  textAlign: "right", 
+  height: '2em', 
+  padding: theme.spacing(2), 
+  fontSize: "4em", 
+  overflow: "hidden"
+
+}))
 const CalculatorBase = styled(Paper)(({theme}) => ({
   padding: theme.spacing(2),
   marginTop: theme.spacing(4),
@@ -10,7 +19,11 @@ function App() {
   return (
   <Container maxWidth="lg">
      <CalculatorBase elevation={3}> 
-Teste
+<Grid container spacing={1}>
+  <Grid item xs={11}>
+    <OutPutContainer>0</OutPutContainer>
+  </Grid>
+  </Grid>
      </CalculatorBase>
      </Container>
   );
