e615f3f9a215c77d64368cc08f8d2f48db9f0153
ogatalars <ogataogara@gmail.com>
2023-04-28 13:29:39 -0300

    Fix: material UI

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 import App from "./App"
+import theme from './theme';
+
 
 const root= ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
@@ -8,6 +11,9 @@ const root= ReactDOM.createRoot(
 
 root.render(
     <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
         <App />
+        </ThemeProvider>
     </React.StrictMode>
 )
---
+++ src/theme.tsx
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+
+
+const theme = createTheme({
+    palette: {
+        mode: "dark", 
+        primary: {main: "#fff149"}
+        }
+})
+
+export default theme
