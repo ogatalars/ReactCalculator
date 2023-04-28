1fc9210fbcdba0c21ea5bf9edf3a0dc5c000a9bd
ogatalars <ogataogara@gmail.com>
2023-04-28 14:12:45 -0300

    Add: based calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import {Container, Paper, styled} from "@mui/material"
 
+const CalculatorBase = styled(Paper)(({theme}) => ({
+  padding: theme.spacing(2),
+  marginTop: theme.spacing(4),
+  borderRadius : 15
+}))
 function App() {
   return (
-    <div className="App">
-    </div>
+  <Container maxWidth="lg">
+     <CalculatorBase elevation={3}> 
+Teste
+     </CalculatorBase>
+     </Container>
   );
 }
 
