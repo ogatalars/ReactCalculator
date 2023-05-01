42efb0acb19247a17fab3c34ff1ee822f618b693
ogatalars <ogataogara@gmail.com>
2023-05-01 11:01:59 -0300

    Add: the ac button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import {useState} from 'react';
 import {Container, Paper,Grid, styled} from "@mui/material"
+import { DigitalOperationalButton } from './DigitalOperationalButton';
 
 const OutPutContainer = styled('div')(({theme})=> ({
   width: "90%", 
@@ -16,12 +17,29 @@ const CalculatorBase = styled(Paper)(({theme}) => ({
   borderRadius : 15
 }))
 function App() {
+  const [currentValue, setCurrentValue] = useState("0")
+  const [operation, setOperation] = useState("")
+  const selectOperation = (operation:string) => {
+  setOperation(operation)
+  }
+
+  const setDigit  = (digit: string) => {
+    setCurrentValue(digit)
+  }
+
   return (
   <Container maxWidth="lg">
      <CalculatorBase elevation={3}> 
 <Grid container spacing={1}>
   <Grid item xs={11}>
-    <OutPutContainer>0</OutPutContainer>
+    <OutPutContainer>{currentValue}</OutPutContainer>
+  </Grid>
+  <Grid  item container columnSpacing={1}>
+  <DigitalOperationalButton 
+  operational={"AC"}
+  selectOperation={selectOperation}
+  selectedOperation={operation}
+  />
   </Grid>
   </Grid>
      </CalculatorBase>
